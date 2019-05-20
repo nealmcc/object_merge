ccbe6f7cca321aaefa1587d63ea64a296cc767b0
Neal McConachie <neal@mcconachie.co>
2019-05-20 21:01:01 +0100

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="object_merge",
+    version="0.1",
+    description="Basic user-guided object merging library",
+    author="Neal McConachie",
+    author_email="neal@mcconachie.co",
+    url="https://github.com/nealmcc",
+    packages=find_packages()
+)
